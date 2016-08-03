1d2a844a7a3cd6ef8367dfa9e4b00ff068726e3b
Pete Sellars <psellars@gmail.com>
2016-08-03 19:22:23 +1200

    added test file

---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+(
+# find all executables and run shellcheck
+for f in $(find . -type f -excutable); do
+	shellcheck $f && echo -e "--\nSuccessfully linted $f\n--"
+done
+) || true
