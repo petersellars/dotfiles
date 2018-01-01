2f96159266bef679c65c78404a055ba7f2fd9be3
Peter Sellars <psellars@gmail.com>
2018-01-01 19:02:07 +1300

    added sudo capabilities and additional base and basemin functionality

--- bin/install.sh
+++ bin/install.sh
@@ -42,9 +42,18 @@ setup_sources_min() {
 	deb-src http://ppa.launchpad.net/git-core/ppa/ubuntu xenial main
 	EOF
 
+	# neovim
+	cat <<-EOF > /etc/apt/sources.list.d/neovim.list
+	deb http://ppa.launchpad.net/neovim-ppa/unstable/ubuntu xenial main
+	deb-src http://ppa.launchpad.net/neovim-ppa/unstable/ubuntu xenial main
+	EOF
+
 	# Add the git-core ppa gpg key
 	apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys E1DD270288B4E6030699E45FA1715D88E1DF1F24
 
+	# Add the neovim ppa gpg key
+	apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 9DBB0BE9366964F134855E2255F96FCF8231B6DD
+	
 	# Turn off translations, speed up apt update
 	mkdir -p /etc/apt/apt.conf.d
         echo 'Acquire::Languages "none";' > /etc/apt/apt.conf.d/99translations
@@ -54,6 +63,38 @@ setup_sources_min() {
 # Assumes you are going to use debian stretch
 setup_sources() {
 	setup_sources_min;
+
+	cat <<-EOF > /etc/apt/sources.list
+	deb http://httpredir.debian.org/debian stretch main contrib non-free
+	deb-src http://httpredir.debian.org/debian/ stretch main contrib non-free
+
+	deb http://httpredir.debian.org/debian stretch-updates main contrib non-free
+	deb-src http://httpredir.debian.org/debian/ stretch-updates main contrib non-free
+
+	deb http://security.debian.org/ stretch/updates main contrib non-free
+	deb-src http://security.debian.org/ stretch/updates main contrib non-free
+
+	#deb http://httpredir.debian.org/debian/ jessie-backports main contrib non-free
+	#deb-src http://httpredir.debian.org/debian/ jessie-backports main contrib non-free
+
+	deb http://httpredir.debian.org/debian experimental main contrib non-free
+	deb-src http://httpredir.debian.org/debian experimental main contrib non-free
+
+	# yubico - Uncomment this when I have a yubikey
+	# https://www.yubico.com/
+	#deb http://ppa.launchpad.net/yubico/stable/ubuntu xenial main
+	#deb-src http://ppa.launchpad.net/yubico/stable/ubuntu xenial main
+
+	# tlp: Advanced Linux Power Management
+	# http://linrunner.de/en/tlp/docs/tlp-linux-advanced-power-management.html
+	deb http://repo.linrunner.de/debian sid main
+	EOF
+
+	# Add the yubico ppa gpg key
+	#apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 3653E21064B19D134466702E43D5C49532CBA1A9
+
+	# Add the tlp apt-repo gpg key
+	apt-key adv --keyserver pool.sks-keyservers.net --recv-keys CD4E8809
 }
 
 base_min() {
@@ -61,7 +102,11 @@ base_min() {
 	apt -y upgrade
 
 	apt install -y \
+		adduser \
 		git \
+                make \
+		neovim \
+		sudo \
 		--no-install-recommends
 
 	apt autoremove
@@ -80,10 +125,13 @@ base() {
 	apt -y upgrade
 
 	apt install -y \
-		git \
+		apparmor \
 		--no-install-recommends
 
-	#setup_sudo
+	# Install tlp with recommends
+	apt install -y tlp tlp-rdw
+
+	setup_sudo
 
 	apt autoremove
 	apt autoclean
@@ -92,10 +140,32 @@ base() {
 	#install_docker
 }
 
+# Setup sudo for a user
+# because fuck typing that shit all the time
+# just have a decent password
+# and lock your computer when you aren't using it
+# if they have your password they can sudo anyways
+# so its pointless
+# I know what the fuck I'm doing ;)
+setup_sudo() {
+	# Add user to sudoers
+	adduser "$TARGET_USER" sudo
+
+	# Add user to systemd groups
+	# then you won't need sudo to view logs and shit
+	gpasswd -a "$TARGET_USER" systemd-journal
+	gpasswd -a "$TARGET_USER" systemd-network
+
+	{ \
+		echo -e "${TARGET_USER} ALL=(ALL) NOPASSWD:ALL"; \
+	} >> /etc/sudoers
+}
+
 usage() {
 	echo -e "install.sh\\n\\tThis script installs my basic setup for a debian laptop or vm\\n"
 	echo "Usage:"
-	echo "  base				    - setup sources & install base pkgs"
+	echo "  base                           - setup sources & install base pkgs"
+	echo "  basemin                        - setup sources & install base min pkgs"
 }
 
 main() {
@@ -114,6 +184,14 @@ main() {
 		setup_sources
 
 		base
+	elif [[ $cmd == "basemin" ]]; then
+		check_is_sudo
+		get_user
+
+		# Set up /etc/apt/sources.list
+		setup_sources_min
+
+		base_min
 	else
 		usage
 	fi
